5686a2d1fd996f14b626e15a012bf1cfef8c6682
Simon <simibac93@gmail.com>
2020-05-02 13:31:28 +0200

    responsive progress circles

--- src/components/ProgressProperty.js
+++ src/components/ProgressProperty.js
@@ -5,33 +5,37 @@ import {
 } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 
-import { Typography } from "@material-ui/core";
+import {Typography} from "@material-ui/core";
+import withWidth from '@material-ui/core/withWidth';
 
-export default function ProgressProperty({ progress, emoji, property, color }) {
+function ProgressProperty({progress, emoji, property, color, width}) {
   return (
-    <div
-      style={
-        {
-          // margin: "20px"
-        }
-      }
+    <CircularProgressbarWithChildren
+      value={progress}
+      styles={buildStyles({
+        pathColor: color,
+      })}
     >
-      <CircularProgressbarWithChildren
-        value={progress}
-        styles={buildStyles({
-          pathColor: color,
-        })}
-      >
-        <Typography variant="body1">
-          <span role="img" aria-label="emoji" style={{ fontSize: "30px" }}>
+      <Typography variant="body1">
+          <span role="img" aria-label="emoji" style={{fontSize: "30px"}}>
             {emoji}
           </span>
-        </Typography>
+      </Typography>
+      {
+        width !== 'xs' &&
         <Typography variant="body1">{property}</Typography>
+
+      }
+      {
+        width !== 'xs' &&
         <Typography variant="body1">
           <strong>{progress}%</strong>
         </Typography>
-      </CircularProgressbarWithChildren>
-    </div>
+      }
+
+
+    </CircularProgressbarWithChildren>
   );
 }
+
+export default withWidth()(ProgressProperty)
