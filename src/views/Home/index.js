c0ac68f7f05c70c97c7fab2fc4caac81d2a5ba91
Simon <simibac93@gmail.com>
2020-05-02 13:34:30 +0200

    responsive home image

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -4,7 +4,9 @@ import logo from "./PokeMate.png";
 export default function Home() {
   return (
     <div className="content">
-      <img src={logo} alt="" />
+      <img src={logo} alt="" style={{
+        maxWidth: "100%", height: "auto"
+      }}/>
       <p>version_1.0</p>
     </div>
   );
